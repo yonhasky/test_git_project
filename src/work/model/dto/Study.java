c63a96aedd3c2e3b31363d01b73cd1f7b190be7c
Format <Format@Format-PC>
2016-10-03 02:31:43 +0900

    add study

---
+++ src/work/model/dto/Study.java
@@ -0,0 +1,35 @@
+package work.model.dto;
+
+public class Study {
+	int stNo;
+	String stTitle;
+	String stAuthor;
+	String stDate;
+	String stPeriod;
+	String stOverview;
+	String stContent;
+	String stFile1;
+	String stFile2;
+	int stHit;
+	String stStatus;
+	public Study() {
+		super();
+	}
+	public Study(int stNo, String stTitle, String stAuthor, String stDate, String stPeriod, String stOverview,
+			String stContent, String stFile1, String stFile2, int stHit, String stStatus) {
+		super();
+		this.stNo = stNo;
+		this.stTitle = stTitle;
+		this.stAuthor = stAuthor;
+		this.stDate = stDate;
+		this.stPeriod = stPeriod;
+		this.stOverview = stOverview;
+		this.stContent = stContent;
+		this.stFile1 = stFile1;
+		this.stFile2 = stFile2;
+		this.stHit = stHit;
+		this.stStatus = stStatus;
+	}
+	
+	
+}
