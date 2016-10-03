070ee8c938a89e06695d5f1d25f166c87e0eb826
Format <Format@Format-PC>
2016-10-03 16:48:09 +0900

    Add Study & Mentoring

--- src/work/model/dto/Student.java
+++ src/work/model/dto/Student.java
@@ -1,6 +1,27 @@
 package work.model.dto;
 
-public class Student {
+import java.io.Serializable;
+/*
+ * CREATE TABLE STUDENTS(
+ID       varchar2(20) primary key,   -- 아이디
+PW       varchar2(20) not null,      -- 비밀번호
+NAME       varchar2(20) not null,    -- 이름
+MOBILE       varchar2(15) not null,  -- 연락처
+EMAIL       varchar2(30) not null,   -- 이메일
+BIRTH       number(4) not null,      -- 출생년(1991)
+GENDER         varchar2(5) not null, -- 성별 (남자 M, 여자 F)
+MAJOR        varchar2(20) ,          -- 전공
+DIVISION    varchar2(20) ,           -- 세부전공
+GRADE       varchar2(10) ,           -- 학번(98, 10 ...)
+IMG       varchar2(100) ,            -- 이미지
+GDATE       varchar2(15) ,           -- 졸업일(2010년 02월)
+COMPANY       varchar2(30) ,         -- 근무회사        
+JOB       varchar2(30) ,             -- 직종 ( IT계열 / 화학 ... )
+CAREER       varchar2(20) ,          -- 경력(3년 6개월)
+PART       varchar2(10) not null    -- 회원등급(관리자 A , 재학생 S, 졸업생 G)
+);
+ */
+public class Student implements Serializable{
 
 	private String id;
 	private String pw;
@@ -22,24 +43,13 @@ public class Student {
 	// 학생, 졸업생, 관리자 구분
 	private String part;
 
-	public Student() {
-	}
+	public Student() { }
 
-	
-	
-	
-	
-	
 	public Student(String id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
-
-
-
-
-
 
 	/**
 	 * 재학생 회원
@@ -113,11 +123,6 @@ public class Student {
 		this.career = career;
 	}
 
-
-
-
-
-
 	/**
 	 * 전체 초기화 생성자
 	 * @param id
@@ -160,10 +165,7 @@ public class Student {
 		this.part = part;
 	}
 
-	
-	
-	
-	
+		
 	public String getId() {
 		return id;
 	}
@@ -292,9 +294,6 @@ public class Student {
 		this.part = part;
 	}
 
-	
-
-	
 
 	@Override
 	public int hashCode() {
@@ -319,10 +318,6 @@ public class Student {
 		result = prime * result + ((pw == null) ? 0 : pw.hashCode());
 		return result;
 	}
-
-
-
-
 
 
 	@Override
@@ -415,10 +410,6 @@ public class Student {
 	}
 
 
-
-
-
-
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -457,5 +448,4 @@ public class Student {
 		builder.append("]");
 		return builder.toString();
 	}
-
 }
---
+++ src/work/model/service/StudyService.java
@@ -0,0 +1,10 @@
+package work.model.service;
+
+import work.model.dao.FactoryDao;
+
+public class StudyService {
+	FactoryDao factory = FactoryDao.getInstance();
+	
+	
+}
+ 
